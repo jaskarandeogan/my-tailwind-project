be17db27edc2b851190dda7ba99ebfd1ce7784e2
Jaskaranvir Deogan <84167202+jaskarandeogan@users.noreply.github.com>
2022-12-20 19:22:35 -0800

    add modal

--- my-tailwind-project/src/components/FlagCard.jsx
+++ my-tailwind-project/src/components/FlagCard.jsx
@@ -1,16 +1,24 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { formatNumbers } from "../vanilla/formatNumbers";
+import Modals from "./shared/Modals";
+import FlagModal from "./FlagModal";
 const FlagCard = ({ result, index }) => {
+  const [open, setOpen] = useState(false);
+
+  function handleModal() {
+    setOpen(true);
+  }
+
   return (
     <>
       {/* write html here for cards */}
       <div
         key={index}
-        className="max-w-[16.5rem] dark:bg-gradient-to-tl from-[#28313B] to-[#485461] transition group bg-white dark:bg-indigo-200 hover:scale-100 hover:drop-shadow-2xl border-2 border-grey-200 dark:border-3 dark:border-orange-700 hover:border-red-200 hover:cursor-pointer "
+        className="w-[16.5rem] dark:bg-gradient-to-tl from-[#28313B] to-[#485461] transition group bg-white dark:bg-indigo-200 hover:scale-100 hover:drop-shadow-2xl border-2 border-grey-200 dark:border-3 dark:border-orange-700 hover:border-red-200 hover:cursor-pointer "
       >
         <div>
           <img
-            className="h-36 w-full m-auto object-cover opacity-50 group-hover:opacity-100"
+            className="h-40 w-full m-auto object-cover opacity-50 group-hover:opacity-100"
             alt="flag"
             src={result.flags.png}
           />
@@ -19,22 +27,28 @@ const FlagCard = ({ result, index }) => {
           {result.name.common}
         </h4>
         <p className="mx-4 my-2 dark:text-gray-300">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi
-          exercitationem aliquid omnis facere consectetur odit quibusdam
-          inventore eos, magni nostrum.
+          <span className="font-medium">Capital City: </span> {result.capital}
         </p>
         <p className="mx-4 my-2 dark:text-gray-300">
-          Population: {result.population}
+          <span className="font-medium">Population: </span>
+          {formatNumbers(result.population)}
         </p>
-        <p className="my-4 mx-4 dark:text-gray-300">Region: {result.region}</p>
+        <p className="mx-4 my-2 dark:text-gray-300">
+          <span className="font-medium">Region: </span>
+          {result.region}
+        </p>
         <div className="flex justify-center ">
           <button
-            className="bg-blue-700 mb-4 px-4 py-2 text-white  font-bold
+            className="bg-blue-700 m-5 px-4 py-2 text-white  font-bold
                 dark:bg-gradient-to-tl from-[#2b5876] to-[#4e4376] dark:text-gray-300
-                "
+                hover:bg-blue-400 transition duration-300 ease-in-out rounded-sm"
+            onClick={() => handleModal()}
           >
             View More
           </button>
+          <Modals open={open} setOpen={setOpen} >
+            <FlagModal result={result} />
+          </Modals>
         </div>
       </div>
     </>
